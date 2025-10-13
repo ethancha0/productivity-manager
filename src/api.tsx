7f97b0b6c1814f3f2e20de8c7578d45d170afd65
ethancha0 <ethanchao2005@gmail.com>
2025-10-13 13:42:12 -0700

    updated api

--- src/api.tsx
+++ src/api.tsx
@@ -1,4 +1,4 @@
 
 export const API = import.meta.env.DEV
   ? "http://127.0.0.1:5000"              // local Flask
-  : "https://dashboard.render.com/web/srv-d3lk2bogjchc73cfgln0"; // prod Flask
+  : "https://productivity-manager-iwb3.onrender.com"; // prod Flask
